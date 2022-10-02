fdbba380cb10780d131065b46f03766eb21bfa79
Afran-rafi <fahimrrrrr@gmail.com>
2022-10-03 03:37:50 +0600

    carousel swiper done

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     ];
     let [open, setOpen] = useState(false);
     return (
-        <div className='sticky shadow-md top-0 left-0 z-50 font-[Poppins] py-6 text-center bg-indigo-500 lg:px-24 px-4'>
+        <div className='sticky shadow-md top-0 left-0 z-50 font-[Poppins] py-6 text-center bg-gradient lg:px-24 px-4'>
             <div className='flex items-center justify-between'>
                 <div className='cursor-pointer'>
                     <Link to="/"><img src={logo} alt="" /></Link>
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <div onClick={() => setOpen(!open)} className='text-2xl absolute right-6 top-6 cursor-pointer lg:hidden text-white'>
                     <i className={`${open ? 'fa-solid fa-x' : 'fa-solid fa-bars'}`}></i>
                 </div>
-                <ul className={`lg:flex lg:items-center lg:pb-0 pb-8 absolute lg:static bg-indigo-500 lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-7 transition-all duration-500 ease-in ${open ? 'top-16 opacity-100' : 'top-[-490px] opacity-0'} lg:opacity-100`}>
+                <ul className={`lg:flex lg:items-center lg:pb-0 pb-8 absolute lg:static bgg lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-7 transition-all duration-500 ease-in ${open ? 'top-16 opacity-100' : 'top-[-490px] opacity-0'} lg:opacity-100`}>
                     {
                         Links.map(link => <li
                             className='px-4 text-xl lg:my-0 my-5 text-white'
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -5,6 +5,8 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 
+import '../CSS/Home.css'
+
 import banner1 from '../Images/banner1.jpg'
 import banner2 from '../Images/banner2.jpg'
 import banner3 from '../Images/banner3.jpg'
@@ -12,7 +14,7 @@ import banner3 from '../Images/banner3.jpg'
 
 const Home = () => {
     return (
-        <div className='lg:px-24 px-4 lg:h-[86.2vh] bg-indigo-500 flex justify-center items-center'>
+        <div className='lg:px-24 px-4 lg:h-[87vh] lg:py-0 py-8 bg-gradient flex justify-center items-center'>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -30,12 +32,12 @@ const Home = () => {
                 <SwiperSlide>
                     <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-10 gap-6 items-center'>
                         <div className='lg:-order-none order-6'>
-                            <h2 className='lg:text-6xl text-2xl font-bold'>We Are Qualified & Professional</h2>
-                            <p className='mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
-                            <button className='bg-indigo-600 px-6 py-2 text-white rounded mt-4 font-bold'>Discover More</button>
+                            <h2 className='lg:text-6xl text-4xl font-bold text-gray-200'>We Are Qualified & Professional</h2>
+                            <p className='mt-4 text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
+                            <button className='bg-[#ff136f] shadow-lg px-6 py-2 text-white rounded mt-4 font-bold'>Discover More <i className="fa-solid fa-car-side"></i></button>
                         </div>
                         <div>
-                            <div className='flex lg:justify-end justify-center'>
+                            <div className='flex lg:justify-end justify-center lg:mt-0'>
                                 <img className='image rounded' src={banner1} alt="" />
                             </div>
                         </div>
@@ -44,12 +46,12 @@ const Home = () => {
                 <SwiperSlide>
                     <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-10 gap-6 items-center'>
                         <div className='lg:-order-none order-6'>
-                            <h2 className='lg:text-6xl text-2xl font-bold'>100% Best Quality Car Services</h2>
-                            <p className='mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
-                            <button className='bg-indigo-600 px-6 py-2 text-white rounded mt-4 font-bold'>Discover More</button>
+                            <h2 className='lg:text-6xl text-4xl font-bold text-gray-200'>100% Best Quality Car Services</h2>
+                            <p className='mt-4 text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
+                            <button className='bg-[#ff136f] shadow-lg px-6 py-2 text-white rounded mt-4 font-bold'>Discover More <i className="fa-solid fa-car-side"></i></button>
                         </div>
                         <div>
-                            <div className='flex lg:justify-end justify-center'>
+                            <div className='flex lg:justify-end justify-center lg:mt-0'>
                                 <img className='image rounded' src={banner2} alt="" />
                             </div>
                         </div>
@@ -58,12 +60,12 @@ const Home = () => {
                 <SwiperSlide>
                     <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-10 gap-6 items-center'>
                         <div className='lg:-order-none order-6'>
-                            <h2 className='lg:text-6xl text-2xl font-bold'>Car is serviced by experts Persons.</h2>
-                            <p className='mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
-                            <button className='bg-indigo-600 px-6 py-2 text-white rounded mt-4 font-bold'>Discover More</button>
+                            <h2 className='lg:text-6xl text-4xl font-bold text-gray-200'>Car is serviced by experts Persons.</h2>
+                            <p className='mt-4 text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi et tempore accusantium modi sit dolor ipsum maiores illo! Tempora quasi asperiores vitae enim minus!</p>
+                            <button className='bg-[#ff136f] shadow-lg px-6 py-2 text-white rounded mt-4 font-bold'>Discover More <i className="fa-solid fa-car-side"></i></button>
                         </div>
                         <div>
-                            <div className='flex lg:justify-end justify-center'>
+                            <div className='flex lg:justify-end justify-center lg:mt-0'>
                                 <img className='image rounded' src={banner3} alt="" />
                             </div>
                         </div>
