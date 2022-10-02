f2ffd06d2c7a5d9c18676884f9d563a40f4f2c0b
Afran-rafi <fahimrrrrr@gmail.com>
2022-10-03 03:53:56 +0600

    firebase confiq and our service start

--- src/Components/pages/HomePage.js
+++ src/Components/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './Home';
+import Services from './Services';
 
 const HomePage = () => {
     return (
         <div>
             <Home></Home>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Components/pages/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='lg:px-24 px-4 my-6'>
+            <h1 className='text-center text-4xl font-bold text-gray-800'>Our Services</h1>
+        </div>
+    );
+};
+
+export default Services;
